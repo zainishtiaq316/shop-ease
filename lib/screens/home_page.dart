3c4b3904b2d4d43ccbc26d949fd00b71068d23d3
Zain Ishtiaq <zainishtiaq.7866@gmail.com>
2024-05-29 14:14:08 +0500

    updated

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-
-import 'package:shopease/models/user-model.dart';
 import 'package:shopease/screens/auth-ui/splash-screen.dart';
 import 'package:shopease/widgets/custom-drawer-widget.dart';
 
@@ -16,16 +13,12 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
-  final _pageControlller = PageController();
-  User? user = FirebaseAuth.instance.currentUser;
-
   late FocusNode myFocusNode;
 
   DateTime? currentBackPressTime;
-  int selectedDate = 1;
+  int _selectedIndex = 0;
 
   Future<bool> onWillPop() async {
-    // myFocusNode.unfocus();
     DateTime now = DateTime.now();
     if (currentBackPressTime == null ||
         now.difference(currentBackPressTime!) > Duration(seconds: 2)) {
@@ -44,86 +37,132 @@ class _HomePageViewState extends State<HomePageView> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    myFocusNode = FocusNode();
+  }
+
+  @override
   void dispose() {
     super.dispose();
-    _pageControlller.dispose();
     myFocusNode.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    int _selectedIndex = 0;
-   const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
-   const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Likes',
-      style: optionStyle,
-    ),
-    Text(
-      'Search',
-      style: optionStyle,
-    ),
-    Text(
-      'Profile',
-      style: optionStyle,
-    ),
-  ];
+    const TextStyle optionStyle =
+        TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+    const List<Widget> _widgetOptions = <Widget>[
+      Text(
+        'Home',
+        style: optionStyle,
+      ),
+      Text(
+        'Likes',
+        style: optionStyle,
+      ),
+      Text(
+        'Search',
+        style: optionStyle,
+      ),
+      Text(
+        'Profile',
+        style: optionStyle,
+      ),
+    ];
+
     return WillPopScope(
         onWillPop: onWillPop,
         child: Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 20,
-        title: const Text('GoogleNavBar'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: Container(
-        color: Colors.purple.shade100,
-       padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
-       
-        child: GNav(
-          backgroundColor: Colors.purple.shade100,
-          rippleColor: Colors.grey[300]!,
-          hoverColor: Colors.grey[100]!,
-          gap: 8,
-          activeColor: Colors.black,
-          iconSize: 24,
-          padding: EdgeInsets.all(16),
-          duration: Duration(milliseconds: 400),
-          tabBackgroundColor: Colors.grey[100]!,
-          color: Colors.black,
-          tabs: [
-            GButton(
-              icon: Icons.home,
-              text: 'Home',
+          backgroundColor: Colors.white,
+          drawer: DrawerWidget(),
+          appBar: AppBar(
+            title: Text(
+              _selectedIndex == 0
+                  ? 'Home'
+                  : _selectedIndex == 1
+                      ? 'Likes'
+                      : _selectedIndex == 2
+                          ? 'Search'
+                          : 'Profile',
             ),
-            GButton(
-              icon: Icons.favorite,
-              text: 'Likes',
+            actions: _selectedIndex == 0
+                ? [
+                    IconButton(
+                      icon: Icon(Icons.shopping_cart),
+                      onPressed: () {
+                        // Handle cart action
+                      },
+                    ),
+                  ]
+                : [],
+          ),
+          body: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+          bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.purple, Colors.pink],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(30.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10,
+                  spreadRadius: 1,
+                ),
+              ],
             ),
-            GButton(
-              icon: Icons.search,
-              text: 'Search',
+            margin: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+              child: GNav(
+                backgroundColor: Colors.transparent,
+                rippleColor: Colors.grey[300]!,
+                hoverColor: Colors.grey[100]!,
+                gap: 8,
+                activeColor: Colors.white,
+                iconSize: 24,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                duration: Duration(milliseconds: 400),
+                tabBackgroundColor: Colors.white24,
+                color: Colors.white,
+                tabs: [
+                  GButton(
+                    icon: Icons.home,
+                    text: 'Home',
+                    iconColor: Colors.white,
+                  ),
+                  GButton(
+                    icon: Icons.favorite,
+                    text: 'Likes',
+                    iconColor: Colors.white,
+                  ),
+                  GButton(
+                    icon: Icons.search,
+                    text: 'Search',
+                    iconColor: Colors.white,
+                  ),
+                  GButton(
+                    icon: Icons.person,
+                    text: 'Profile',
+                    iconColor: Colors.white,
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+              ),
             ),
-            GButton(
-              icon: Icons.person,
-              text: 'Profile',
-            ),
-          ],
-          selectedIndex: _selectedIndex,
-          onTabChange: (index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-        ),
-      ),
-    )); }
+          ),
+        
+        
+        ));
+  }
 }
